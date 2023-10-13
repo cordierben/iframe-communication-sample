4e5a8941aa70dea09ce33cab05436df293c9b9a6
AL1088458_totalen <b.cordier@groupeonepoint.com>
2023-10-13 15:45:00 +0200

    Add hostElement

--- src/app/iframe-container/iframe-container.component.ts
+++ src/app/iframe-container/iframe-container.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var _flutter: any;
+
 @Component({
   selector: 'app-iframe-container',
   templateUrl: './iframe-container.component.html',
   styleUrls: ['./iframe-container.component.css']
 })
 export class IframeContainerComponent implements OnInit {
-  iframeHeight: number = 300; // Default height for the iframe
 
   constructor() { }
 
   ngOnInit(): void {
     // Listen to the event emitted by Flutter
-    window.addEventListener('message', (event) => {
-      const flutterEvent = JSON.parse(event.data);
-      if (flutterEvent.type == "pageHeight") {
-        // Set the iframe height based on the received data from Flutter
-        console.log("ui");
-        this.iframeHeight = flutterEvent.pageHeight;
-      }
+    console.log("Hello");
+    window.addEventListener("load", function (ev) {
+    _flutter.loader.loadEntrypoint({
+    entrypointUrl: "../../assets/flutter/main.dart.js",
+        onEntrypointLoaded: async function(engineInitializer: any) {
+          let appRunner = await engineInitializer.initializeEngine({
+            // Pass a reference to "div#flutter_host" into the Flutter engine.
+            hostElement: document.querySelector("#flutter_host")
+          });
+          await appRunner.runApp();
+        }
+      });
     });
   }
 }
